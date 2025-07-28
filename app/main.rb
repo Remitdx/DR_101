dc29fc4bc0bc4defd51d7e335e1171bce601b1d7
kodoliprane <remi.tondeux@gmail.com>
2025-07-28 16:28:21 +0200

    tuto 5/11

--- app/main.rb
+++ app/main.rb
@@ -1,20 +1,15 @@
-def tick args
-  args.state.player ||= { x: 576,
-                          y: 200,
-                          w: 100,
-                          h: 100,
-                          speed: 10,
-                          path: 'mygame/sprites/dwarf.png' }
+def spawn_target
+  size = 64
+  {
+    x: rand(1200),
+    y: rand(200),
+    w: size,
+    h: size,
+    path: 'sprites/macron.png'
+  }
+end
 
-  args.state.balls ||= []
-
-  args.outputs.labels  << { x: 640,
-                            y: 600,
-                            text: 'Hello Rémi!',
-                            size_px: 30,
-                            anchor_x: 0.5,
-                            anchor_y: 0.5 }
-
+def handle_player_moves args
   if args.inputs.left
     args.state.player.x -= args.state.player.speed
   elsif args.inputs.right
@@ -25,20 +20,6 @@ def tick args
     args.state.player.y -= args.state.player.speed
   elsif args.inputs.up
     args.state.player.y += args.state.player.speed
-  end
-  
-  if args.inputs.keyboard.key_down.space || args.inputs.controller_one.key_down.a
-    args.state.balls << {
-      x: args.state.player.x + 50,
-      y: args.state.player.y - 30,
-      w: 50,
-      h: 50,
-      path: 'mygame/sprites/caca.png'
-    }
-  end
-
-  args.state.balls.each do |ball|
-    ball.y -= 6
   end
 
   if args.state.player.x > 1180
@@ -52,6 +33,82 @@ def tick args
   elsif args.state.player.y < 0
     args.state.player.y = 0
   end
-
-  args.outputs.sprites << [args.state.player, args.state.balls]
 end
+
+def new_target_valid? args, new_target
+  return false if args.geometry.intersect_rect?(args.state.player, new_target)
+
+  args.state.targets.each do |target|
+    if args.geometry.intersect_rect?(new_target, target)
+      return false
+    end
+  end
+  true
+end
+
+def tick args
+  args.state.player ||= { x: 576,
+                          y: 500,
+                          w: 100,
+                          h: 100,
+                          speed: 10,
+                          path: 'mygame/sprites/dwarf.png' }
+
+  args.state.balls ||= []
+  args.state.score ||= 0
+  args.state.targets ||= [
+    spawn_target,
+    spawn_target,
+    spawn_target
+  ]
+
+  args.outputs.labels  << { x: 60,
+                            y: 680,
+                            text: "Score : #{args.state.score}",
+                            size_px: 20,
+                            anchor_x: 0.5,
+                            anchor_y: 0.5 }
+
+  
+  
+  if args.inputs.keyboard.key_down.space || args.inputs.controller_one.key_down.a
+    args.state.balls << {
+      x: args.state.player.x + 25,
+      y: args.state.player.y - 25,
+      w: 50,
+      h: 50,
+      path: 'mygame/sprites/caca.png'
+    }
+  end
+
+  args.state.balls.each do |ball|
+    ball.y -= 6
+
+    if ball.x > args.grid.w || ball.y < 0
+      ball.dead = true
+      next
+    end
+
+    args.state.targets.each do |target|
+      if args.geometry.intersect_rect?(target, ball)
+        target.dead = true
+        ball.dead = true
+        args.state.score += 10
+        new_target = spawn_target
+        until new_target_valid?(args, new_target)
+          new_target = spawn_target
+        end
+        args.state.targets << new_target
+      end
+    end
+  end
+
+  args.state.targets.reject! { |t| t.dead }
+  args.state.balls.reject! { |b| b.dead }
+
+  handle_player_moves args
+
+  args.outputs.sprites << [args.state.player, args.state.targets, args.state.balls]
+end
+
+$gtk.reset
